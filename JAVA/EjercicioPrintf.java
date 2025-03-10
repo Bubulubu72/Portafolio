1b3d42fc2a71307901a50e49a64a7b5c83f42087
Nicolas Teran <123792355+Bubulubu72@users.noreply.github.com>
2025-03-10 14:40:21 -0600

    Add files via upload

---
+++ JAVA/EjercicioPrintf.java
@@ -0,0 +1,31 @@
+
+
+public class EjercicioPrintf {
+
+	public static void main(String[] args) {
+		
+		
+		//System.out.printf("Esta%nes%notra%nlinea");
+		
+		//System.out.printf("5b%n", null);
+		
+		//System.out.printf("%B%n", false);
+		
+		//System.out.printf("’%s’ %n", "hola");
+		
+		System.out.printf("’%S’ %n", "hola");
+		
+		System.out.printf("’%15s’ %n", "hola");
+		
+		System.out.printf("%2.2s %n", "Hola Mundo");
+		
+		System.out.printf("%f%n", 5.1473);
+		//
+		//System.out.printf("%5.2f%n", 5.1473);
+		
+		//System.out.printf("’%5.2e’%n", 5.1473);
+		
+
+	}
+
+}
