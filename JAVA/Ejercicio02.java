3dd5186c8ab2c891bef68181185534b9c3872f53
Nicolas Teran <123792355+Bubulubu72@users.noreply.github.com>
2025-03-10 14:22:03 -0600

    Add files via upload

---
+++ JAVA/Ejercicio02.java
@@ -0,0 +1,102 @@
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		System.out.println(2 + 3);
+		System.out.println();
+		System.out.println(5 + 4 - 3);
+		System.out.println();
+		System.out.println(-4);
+		System.out.println();
+		System.out.println( (-2 + 5) * (-3) );
+		//////
+		
+		//System.out.println(15 / 0);
+		
+		System.out.println(3 / 2);
+		System.out.println();
+		System.out.println(10 / 2);
+		System.out.println();
+		System.out.println(5.876500000);
+		System.out.println();
+		System.out.println(5 * 3.0);
+		System.out.println();
+		System.out.println(6 + 2.75);
+		System.out.println();
+		System.out.println(5 / 4);
+		System.out.println();
+		System.out.println(5 / 4f);
+		System.out.println();
+		System.out.println(5 / 4.0);
+		//////
+		
+		
+		//System.out.println(2 ** 3);
+		//System.out.println(16 ** (1/2));
+		System.out.println(Math.pow(2, 3));
+		System.out.println();
+		System.out.println(Math.pow(9, 0.5));
+		System.out.println();
+		System.out.println(13 % 2);
+		///////
+		*/
+		
+		System.out.println("Hello world");
+		
+		System.out.println("Hello \nworld");
+		
+		System.out.println("\tHello \tWorld");
+		
+		System.out.println("\t\tHello \rWorld");
+		
+		System.out.println("Hello\bWorld");
+		
+		System.out.println("Hello \" World\"");
+		
+		/*
+		int x1; 
+	     
+        
+	    x1 = 2147483647; 
+	         
+	    x1++; 
+	    System.out.println(x1); 
+	    System.out.println();
+	    
+	    ////
+	    
+	    long x; 
+	    
+        
+	    x = 2147483647; 
+	         
+	    x++; 
+	    System.out.println(x); 
+	    
+	    
+	    ////
+	    
+	    int iNumber1 = 5; 
+	    int iNumber2 = 6; 
+	    float fNumber = iNumber1 /iNumber2; 
+	    System.out.println(fNumber); 
+	    
+	    
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println();
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println();
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println();
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		*/
+	}
+
+}
