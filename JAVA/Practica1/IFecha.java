a241b291c38318d04520f534f0a84cf2e4642809
Nicolas Teran <123792355+Bubulubu72@users.noreply.github.com>
2025-03-10 14:56:00 -0600

    Update

--- JAVA/Practica1/IFecha.java
+++ JAVA/Practica1/IFecha.java
@@ -1,15 +1,15 @@
-package mx.iteso.examen2;
+package practica1;
 
 import java.util.Calendar;
 
 public interface IFecha {
 
 	public int DIA_DEL_MES = Calendar.DAY_OF_MONTH, 
-			   MES_DEL_AÑO = Calendar.MONTH, 
-			   AÑO = Calendar.YEAR;
+			   MES_DEL_AÃ‘O = Calendar.MONTH, 
+			   AÃ‘O = Calendar.YEAR;
 	
 	public int Dia();
 	public int mes();
-	public int año();
+	public int aÃ±o();
 }
 
