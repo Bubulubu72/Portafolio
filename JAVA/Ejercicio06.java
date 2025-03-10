6808be39775a8deb5133f997defaf59200734444
Nicolas Teran <123792355+Bubulubu72@users.noreply.github.com>
2025-03-10 14:33:47 -0600

    Add files via upload

---
+++ JAVA/Ejercicio06.java
@@ -0,0 +1,65 @@
+import javax.swing.JOptionPane;
+
+@SuppressWarnings("unused")
+public class Ejercicio06 {
+
+	/*
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+		 
+		
+		// Ejercicio 1 //
+		int	r , c ;
+		
+		String enter = JOptionPane.showInputDialog("Cuanto mide los renglones: ");
+		 r = Integer.parseInt(enter);
+		 enter = JOptionPane.showInputDialog("Cuanto mide las columnas: ");
+		 c = Integer.parseInt(enter);
+		
+		 double A[][] = new double[c][r];
+		 double b[][] = new double[c][r];
+		 
+		 for ( int i = 0; i <= A.length; i++ ) {
+			 for ( int j = 0;  j <= A[i].length; j++) {
+				 A[i][j] = Math.random();}
+		 }
+		 
+		 for ( int i = 0; i <= b.length; i++ ) {
+			 for ( int j = 0;  j <= b[i].length; j++) {
+				 b[i][j] = Math.random();}
+		 }
+		 
+		 double cR [][] = null;
+		 
+		 for ( int i = 0; i <= cR.length; i++ ) {
+			 for ( int j = 0;  j <= cR[i].length; j++) {
+				 cR[i][j] += ( A[i][j] + b[i][j]); }
+		 }
+		 
+		 for ( int i = 0; i <= cR.length; i++ ) {
+			 for ( int j = 0;  j <= cR[i].length; j++) {
+				 System.out.println( "%.4f" + cR[i][j]);
+				 }
+		 }
+		
+		 //Ejercicio 2 //
+		 
+		 @SuppressWarnings("unused")
+		int rango = 0, nums = 0;
+		 
+		 String enter2 = JOptionPane.showInputDialog("Cuantos son los numeros a calcular: ");
+		 nums = Integer.parseInt(enter2);
+		 
+		 enter2 = JOptionPane.showInputDialog("Cuanto es el rango: ");
+		 rango = Integer.parseInt(enter2);
+		 
+		 double v [] = new double[rango];
+		 
+		 for( int i = 0;  i <= v.length; i++) {
+			 v[i] = Math.random(); }
+		 
+		 
+		 
+	}
+	*/
+}
